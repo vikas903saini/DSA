0705696abb1a7ac79a1ba553c31ff8585c9e3df0
vikas903saini <vikas.903saini@gmail.com>
2024-06-07 11:41:11 -0400

    Bubble Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubbleSort(int arr[],int size){
+    for(int i=0;i<(size-1);i++){
+        for(int j=0;j<(size-i-1);j++){
+            if(arr[j]>arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+}
+void printArr(int arr[],int size){
+    cout<<"Bubble Sort : ";
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<" "; 
+}
+int main(){
+    int arr[]={5,1,2,7,6,3};
+    bubbleSort(arr,6);
+    printArr(arr,6);
+    return 0;
+}
